f71e59e49b29e2de4b3226520649e2077e458ed7
davidv <davidvazquezgit@gmail.com>
2022-11-03 14:33:14 -0700

    add about and home paths

---
+++ main_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Headphones Home Page</h1>')
+
+def about(request):
+  return render(request, 'about.html')
